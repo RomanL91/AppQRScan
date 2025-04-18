17836ad24ca2455bbca8ae3647d5e97eded59e1f
RomanL91 <dev_roman91@mail.ru>
2025-04-18 15:58:02 +0500

    lifespan для создания подключения к rabbitmq

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import uvicorn
 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from views import router as view_router
 from api import router as api_router
 
+from infra.rabbit_depends import (
+    init_broker,
+    connect_broker,
+    disconnect_broker,
+)
 
-app = FastAPI()
+
+# ------------------------------------------------------------------------------------
+# Жизненный цикл приложения (lifespan) вместо on_event("startup") / on_event("shutdown")
+# ------------------------------------------------------------------------------------
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    - до yield: код, выполняющийся "при старте" приложения
+    - после yield: код, выполняющийся "при завершении" приложения
+    """
+    # ── startup ────────────────────────────────────
+    init_broker(app)
+    await connect_broker(app)
+
+    yield
+
+    # ── shutdown ───────────────────────────────────
+    await disconnect_broker(app)
+
+
+# ------------------------------------------------------------------------------------
+# Инициализация приложения FastAPI с помощью lifespan + подключения
+# ------------------------------------------------------------------------------------
+app = FastAPI(
+    title="QRS",
+    lifespan=lifespan,
+)
 app.include_router(router=view_router)
 app.include_router(router=api_router)
 
