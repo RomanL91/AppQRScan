848af2025d6b2911852ac5ab922445c74e3a52c5
RomanL91 <dev_roman91@mail.ru>
2025-04-18 15:59:29 +0500

    модификация ручки принимающая запрос с данными после сканирования qr

--- api/callback_point.py
+++ api/callback_point.py
@@ -1,5 +1,8 @@
 from enum import Enum
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Depends
+
+from core.RabbitInterface import PublisherInterface
+from infra.rabbit_depends import publisher_dep
 
 
 router_callback_point = APIRouter(tags=["callback"])
@@ -18,15 +21,18 @@ async def receive_qr(
     operation: Operation = Query(..., description="ПОГРУЗКА или УПАКОВКА"),
     userId: int | None = Query(None, description="Telegram chat_id"),
     qrData: str = Query(..., description="Содержимое QR‑кода"),
+    publisher: PublisherInterface = Depends(publisher_dep),
 ):
     """
     Простой эндпоинт‑приёмник данных из Web‑страницы/Telegram‑WebApp.
     """
 
-    # здесь можно добавить сохранение в БД, отправку ботом и т. д.
-    return {
-        "status": "ok",
+    payload = {
         "operation": operation,
         "userId": userId,
         "qrData": qrData,
     }
+
+    await publisher.publish(payload)
+
+    return {"status": "queued", **payload}
